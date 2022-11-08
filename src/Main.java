5906122bffba19c672dcab9d793f9b8dc619b0ef
Alex <plarpoon.dev@gmail.com>
2022-11-08 19:20:34 +0100

    changed variable names into more clear ones

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@
  * 3. Sort the numbers
  * 4. Print the numbers first even and then odd
  * 5. Count the number of even and odd numbers
- * 6. Print the number of even and odd numbers
+ * 6. Print the even and odd numbers
  * ===============================================================
  * Alessandro Suha
  * alesuh-1
@@ -23,65 +23,65 @@ import java.util.Random;
 
 class Main {
     public static int RANDOM_NUMBER_GENERATOR(int MIN, int MAX) { // Generates a random number between 1 and 6
-        Random r = new Random();
-        int randomNum = r.nextInt(MAX - MIN);
+        Random random = new Random();
+        int randomNum = random.nextInt(MAX - MIN);
         int result = randomNum + MIN;
         return result;
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         final int MIN = 1; // Minimum number +1 because 0 is neither even nor odd
         final int MAX = 999; // Maximum number
         final String ERROR = "Please enter a positive number higher than 0: ";
 
         int[] array; // Array to store the random numbers
-        int n; // Number of random numbers to be generated
+        int number; // Number of random numbers to be generated
         int evenCount = 0; // Number of even numbers
         int oddCount = 0; // Number of odd numbers
 
         System.out.print("How many random numbers in the range 0 - 999 are desired? ");
 
         // make sure n is a valid integer
-        while (!sc.hasNextInt()) {
+        while (!scanner.hasNextInt()) {
             System.out.print(ERROR);
-            sc.next();
+            scanner.next();
         }
 
         // try catch n is out of memory
         try {
-            n = sc.nextInt();
+            number = scanner.nextInt();
 
             // check if n is bigger than MIN
-            while (n < MIN) {
+            while (number < MIN) {
                 System.out.print(ERROR);
 
-                array = new int[n]; // create an array with n elements
+                array = new int[number]; // create an array with n elements
             }
         } catch (OutOfMemoryError e) {
             System.out.println("Out of memory error. Please enter a smaller number.");
-            sc.close();
+            scanner.close();
             return;
         }
 
         System.out.println(" ");
-        array = new int[n]; // Initializing the array
-        for (int i = 0; i < n; i++) { // Loop to generate n random numbers
+        array = new int[number]; // Initializing the array
+        for (int i = 0; i < number; i++) { // Loop to generate n random numbers
             array[i] = RANDOM_NUMBER_GENERATOR(MIN, MAX);
         }
 
         // Print the array
         System.out.println("Here are the random numbers:");
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < number; i++) {
             System.out.print(array[i] + " ");
         }
 
         System.out.println("\n");
 
         // Bubble sort the array
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
+        for (int i = 0; i < number; i++) {
+            for (int j = i + 1; j < number; j++) {
                 if (array[i] > array[j]) {
                     int temp = array[i];
                     array[i] = array[j];
@@ -92,14 +92,14 @@ class Main {
 
         System.out.println("Here are the random numbers arranged:");
         // Print all the even numbers first and then the odd numbers
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < number; i++) {
             if (array[i] % 2 == 0) {
                 System.out.print(array[i] + " ");
                 evenCount++;
             }
         }
         System.out.print("- ");
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < number; i++) {
             if (array[i] % 2 != 0) {
                 System.out.print(array[i] + " ");
                 oddCount++;
@@ -107,8 +107,8 @@ class Main {
         }
 
         System.out.println(
-                "\n\nOf the above " + n + " numbers, " + evenCount + " were even and " + oddCount + " odd");
+                "\n\nOf the above " + number + " numbers, " + evenCount + " were even and " + oddCount + " odd");
 
-        sc.close();
+        scanner.close();
     }
 }
